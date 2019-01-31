6b4b3ca9ea02919fc5bcd0be9e6639b82d476777
koverg70 <koverg70@gmail.com>
2019-01-31 16:09:21 +0100

    Update entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
 
+_term() {
+  echo "Caught SIGTERM signal! Killing: $child..."
+  kill -SIGTERM "$child" 2>/dev/null
+  wait $child
+  echo "Killed $child."
+}
+
+_int() {
+  echo "Caught SIGINT signal! Killing: $child..."
+  kill -SIGINT "$child" 2>/dev/null
+  wait $child
+  echo "Killed $child."
+}
+
+trap _term SIGTERM
+trap _int SIGINT
+
 set -e
 source /assets/colorecho
 
 if [ ! -d "/opt/oracle/app/product/11.2.0/dbhome_1" ]; then
 	echo_yellow "Database is not installed. Installing..."
-	/assets/install.sh
+        /assets/install.sh &
+        child=$!
+        wait "$child"
 fi
 
-su oracle -c "/assets/entrypoint_oracle.sh"
-
+su oracle -c "/assets/entrypoint_oracle.sh" &
+child=$!
+wait "$child"
